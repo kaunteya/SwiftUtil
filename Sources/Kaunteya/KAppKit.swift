ef7f4eafba3951d59d4f9071eced936305ae3f4d
Kaunteya <>
2020-04-02 12:11:43 +0530

    Updated NSMenu add

--- Sources/Kaunteya/KAppKit.swift
+++ Sources/Kaunteya/KAppKit.swift
@@ -29,10 +29,12 @@ extension NSMenuItem {
 }
 
 extension NSMenu {
-    func add(_ title: String, action: Selector? = nil, keyEquivalent char: String = "", target: AnyObject? = nil, submenu: NSMenu? = nil) {
+    @discardableResult
+    func add(_ title: String, action: Selector? = nil, keyEquivalent char: String = "", target: AnyObject? = nil, submenu: NSMenu? = nil) -> NSMenuItem {
         let item = addItem(withTitle: title, action: action, keyEquivalent: char)
         item.target = target
         item.submenu = submenu
+        return item
     }
 
     func add(_ item: NSMenuItem) {
