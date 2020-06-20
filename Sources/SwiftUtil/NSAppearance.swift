806003d342c3abbf20c6cc6869370231dafaabd0
kaunteya <k.suryawanshi@gmail.com>
2020-06-20 21:55:38 +0530

    Added NSAppearange for darkmode

---
+++ Sources/SwiftUtil/NSAppearance.swift
@@ -0,0 +1,20 @@
+import AppKit.NSAppearance
+
+extension NSAppearance {
+
+    /// if NSAppearance.current.isDark {
+    var isDark: Bool {
+        if self.name == .vibrantDark { return true }
+
+        guard #available(macOS 10.14, *) else { return false }
+
+        switch self.name {
+        case .accessibilityHighContrastDarkAqua,
+             .darkAqua,
+             .accessibilityHighContrastVibrantDark:
+            return true
+        default:
+            return false
+        }
+    }
+}
