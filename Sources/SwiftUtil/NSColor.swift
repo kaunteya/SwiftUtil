978d89399bd0d4404ad507dbe3b3e5769638e6d0
kaunteya <k.suryawanshi@gmail.com>
2020-06-20 16:10:50 +0530

    random to NSColor

--- Sources/SwiftUtil/NSColor.swift
+++ Sources/SwiftUtil/NSColor.swift
@@ -6,4 +6,12 @@ public extension NSColor {
         let blue = CGFloat((hex & 0xFF)) / 255.0
         self.init(calibratedRed: red, green: green, blue: blue, alpha: alpha)
     }
+
+    static var random: NSColor {
+        let red = UInt32.random(in: 0...255)
+        let green = UInt32.random(in: 0...255)
+        let blue = UInt32.random(in: 0...255)
+        let color = NSColor(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: 1)
+        return color
+    }
 }
